2412052786bdd5e38d43c30a42d671682b4b1707
ikaruga <ikaruga777@gmail.com>
2018-07-09 08:16:15 +0900

    tigrc追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,4 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
