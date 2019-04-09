2387a781c388ddaf91b4708258d664f93b1da320
akatsuura <akatsuura@pepabo.com>
2019-04-09 09:56:33 +0900

    pryrcを追加する

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,4 +6,7 @@ ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -fs ~/dotfiles/fish/conf.d/000-env.fish ~/.config/fish/conf.d/000-env.fish
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/dotfiles/.pryrc ~/.pryrc
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/.tmux_conf ~/.tmux_conf
 
