45653998796dc27bb3d95fd4fe0e4b7107742dea
akatsuura <akatsuura@pepabo.com>
2020-03-25 03:17:54 +0900

    fishディレクトリを作ったので、config.fishを移動させる

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,7 +3,7 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
-ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -fs ~/dotfiles/fish/conf.d/000-env.fish ~/.config/fish/conf.d/000-env.fish
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 
