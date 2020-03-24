585ce8ad5b0f04e6443ac3ff847404516d3be6c7
akatsuura <akatsuura@pepabo.com>
2020-03-25 03:07:21 +0900

    000-env.fishのシンボリックリンク

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,6 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -fs ~/dotfiles/fish/conf.d/000-env.fish ~/.config/fish/conf.d/000-env.fish
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+
