9895b896d30159175e74486c0a07481b5d95dc30
akatsuura <akatsuura@pepabo.com>
2020-03-25 03:04:08 +0900

    環境変数っぽいやつをconf.dへ

--- config.fish
+++ config.fish
@@ -1,19 +1,18 @@
-export LANG=ja_JP.UTF-8
-rbenv init - | source
-set -x PATH $HOME/.pyenv/bin $PATH
-set -x PATH $HOME/flutter/bin $PATH
+function fish_prompt
+    $GOPATH/bin/powerline-go -error $status -shell bare \
+         -cwd-max-depth 3 -cwd-max-dir-size -1 \
+        -modules time,nix-shell,venv,ssh,user,cwd,perms,docker,git,jobs,exit,vgo \
+        -newline
+end
 
 function gconfig-ikaruga
   git config --local user.email "ikaruga777@gmail.com"
   git config --local user.name "ikaruga"
   git config --local -l
 end
-export GOPATH=$HOME/go
-function fish_prompt
-    $GOPATH/bin/powerline-go -error $status -shell bare \
-         -cwd-max-depth 3 -cwd-max-dir-size -1 \
-        -modules time,nix-shell,venv,ssh,user,cwd,perms,docker,git,jobs,exit,vgo \
-        -newline
+
+function config_fish
+  vim ~/.config/fish/config.fish
 end
 
 alias g='git'
---
+++ fish/conf.d/000-env.fish
@@ -0,0 +1,8 @@
+set -xg LANG ja_JP.UTF-8
+set -xg LC_ALL ja_JP.UTF-8
+set -x PATH $HOME/.pyenv/bin $PATH
+set -x PATH $HOME/flutter/bin $PATH
+set -x GOPATH $HOME/go
+
+set HISTSIZE 1000000
+
