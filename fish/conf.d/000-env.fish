1564f22c0bb8c32445505b344858c89e7998c636
ikaruga <ikaruga777@gmail.com>
2020-05-18 08:14:51 +0900

    環境変数全部もってくる

--- fish/conf.d/000-env.fish
+++ fish/conf.d/000-env.fish
@@ -1,8 +1,13 @@
 set -xg LANG ja_JP.UTF-8
 set -xg LC_ALL ja_JP.UTF-8
+
+set -x PATH /usr/local/bin $PATH
 set -x PATH $HOME/.pyenv/bin $PATH
 set -x PATH $HOME/flutter/bin $PATH
+set -x PATH $HOME/.cargo/bin $PATH
 set -x GOPATH $HOME/go
 
+set -x PATH $HOME/.nodenv/bin $PATH
+set -x PATH $HOME/.rbenv/bin $PATH
 set HISTSIZE 1000000
 
