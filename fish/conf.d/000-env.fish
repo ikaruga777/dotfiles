dd30efb3a2c7a445efc332f658df9fe5bab723aa
ikaruga <ikaruga777@gmail.com>
2020-07-14 09:45:28 +0900

    GOPATH をPATHに含めていなかった

--- fish/conf.d/000-env.fish
+++ fish/conf.d/000-env.fish
@@ -5,7 +5,9 @@ set -x PATH /usr/local/bin $PATH
 set -x PATH $HOME/.pyenv/bin $PATH
 set -x PATH $HOME/flutter/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
+
 set -x GOPATH $HOME/go
+set -x PATH $GOPATH/bin $PATH
 
 set -x PATH $HOME/.nodenv/bin $PATH
 set -x PATH $HOME/.rbenv/bin $PATH
