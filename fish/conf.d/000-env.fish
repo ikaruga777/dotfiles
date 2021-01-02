ff7688df2fae85480c1339ac86fc50643715fffd
ikaruga <ikaruga777@gmail.com>
2021-01-02 22:34:49 +0900

    gcp sdk の環境変数設定

--- fish/conf.d/000-env.fish
+++ fish/conf.d/000-env.fish
@@ -12,3 +12,5 @@ set -x PATH $GOPATH/bin $PATH
 set -x PATH $HOME/.nodenv/bin $PATH
 set HISTSIZE 1000000
 
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '~/google-cloud-sdk/path.fish.inc' ]; . '~/google-cloud-sdk/path.fish.inc'; end
