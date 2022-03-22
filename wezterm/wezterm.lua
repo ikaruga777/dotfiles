65c1dc7c7c41d73e230dd3b24bfc93589377ae54
ikaruga <ikaruga777@gmail.com>
2022-03-23 08:26:18 +0900

    add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require 'wezterm';
+
+wezterm.on("update-right-status", function(window, pane)
+  -- "Wed Mar 3 08:14"
+  local date = wezterm.strftime("%a %b %-d %H:%M ");
+
+  local bat = ""
+  for _, b in ipairs(wezterm.battery_info()) do
+    bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
+  end
+
+  window:set_right_status(wezterm.format({
+    {Text=bat .. "   "..date},
+  }));
+end)
+
+return {
+  default_prog = {"/usr/local/bin/fish", "-l"},
+  use_ime = true,
+  font = wezterm.font("Cica"),
+  font_size = 16,
+  -- color_scheme = "iceberg-dark",
+  color_scheme = "Jpanesque",
+  -- color_scheme = "JetBrains Darcula",
+ 
+  colors = { compose_cursor = "orange"  },
+  window_background_image_hsb = {
+    brightness = 0.1,
+    hue = 1.0,
+    saturation = 1.0,
+  },
+  keys = {
+    {key="^", mods="CMD", action="IncreaseFontSize"}
+  }
+}
