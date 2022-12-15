eac041c585e46eef538400043488bae28cdb185e
ikaruga <ikaruga777@gmail.com>
2022-12-16 08:25:49 +0900

    nvim はじめました

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -fs ~/dotfiles/fish/conf.d/000-env.fish ~/.config/fish/conf.d/000-env.fish
