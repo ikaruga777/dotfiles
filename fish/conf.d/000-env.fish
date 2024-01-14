f3d2d534dffd37f2d2db523448716ad5b17bdcfa
ikaruga <ikaruga777@gmail.com>
2024-01-14 11:43:18 +0900

    fish pathかえる

--- fish/conf.d/000-env.fish
+++ fish/conf.d/000-env.fish
@@ -2,15 +2,15 @@ set -xg LANG ja_JP.UTF-8
 set -xg LC_ALL ja_JP.UTF-8
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -x PATH /opt/homebrew/bin $PATH
 
 set -x PATH /usr/local/bin $PATH
-set -x PATH $HOME/.pyenv/bin $PATH
 set -x PATH $HOME/flutter/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 
 set -x GOPATH $HOME/go
 set -x PATH $GOPATH/bin $PATH
-rtx activate fish | source
+mise activate fish | source
 
 set HISTSIZE 1000000
 
